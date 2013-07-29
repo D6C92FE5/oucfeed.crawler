48bb5d4f1b7e835cfe484931f52da9a66e02e9e2
D6C92FE5 <D6C92FE5@hotmail.com>
2013-07-29 16:30:58 +0800

    添加部分院系网站的 Spider

---
+++ crawler/oucnews/spiders/yuanxi_gong_cheng.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function, unicode_literals
+
+from oucnews import util
+from oucnews.newsspider import NewsSpider
+
+
+class Spider(NewsSpider):
+    """工程学院
+
+    此网站的内容页链接尾部有一些奇怪的东西
+    栏目“图片新闻”没有抓取
+    党团相关的内容在另一个网站 http://www3.ouc.edu.cn/gongcheng/dangtuan/
+    """
+
+    name = "院系/工程"
+
+    start_urls = [
+        "http://www3.ouc.edu.cn/gongcheng/EG_DynamicsList.aspx?id=55&parentid=122",
+        "http://www3.ouc.edu.cn/gongcheng/EG_DynamicsList.aspx?id=56&parentid=122",
+        "http://www3.ouc.edu.cn/gongcheng/EG_DynamicsList.aspx?id=59&parentid=122",
+        "http://www3.ouc.edu.cn/gongcheng/EG_DynamicsList.aspx?id=68&parentid=122",
+    ]
+
+    list_extract_scope = "//div[@class='nyrightzi']//li"
+    list_extract_field = {
+        'link': ".//@href",
+        'datetime': ".//td[@width='14%']/text()",
+    }
+
+    item_extract_scope = "//div[@class='nyrightzi']"
+    item_extract_field = {
+        'category': "//div[@class='nyrighttopleft']/text()",
+        'title': ".//div[1]/text()",
+        'content': ".//div[2]",
+    }
+
+    datetime_format = "%Y-%m-%d"
+
+    def generate_item_id(self, url):
+        return self.name + util.extract_number(url, 1)
---
+++ crawler/oucnews/spiders/yuanxi_gong_cheng_dang_tuan.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function, unicode_literals
+
+from oucnews import util
+from oucnews.newsspider import NewsSpider
+
+
+class Spider(NewsSpider):
+    """工程学院 党团委
+
+    此网站的内容页链接尾部有一些奇怪的东西
+    只抓取了党团相关的栏目，与工程学院主站重复的栏目没有抓取
+    栏目“文件下载”、“精彩瞬间”没有抓取
+    """
+
+    name = "院系/工程/党团"
+
+    start_urls = [
+        "http://www3.ouc.edu.cn/gongcheng/dangtuan/EG_DynamicsList.aspx?id=151&parentid=126",
+        "http://www3.ouc.edu.cn/gongcheng/dangtuan/EG_DynamicsList.aspx?id=152&parentid=126",
+        "http://www3.ouc.edu.cn/gongcheng/dangtuan/EG_DynamicsList.aspx?id=127&parentid=126",
+        "http://www3.ouc.edu.cn/gongcheng/dangtuan/EG_DynamicsList.aspx?id=128&parentid=126",
+    ]
+
+    list_extract_scope = "//div[@class='nyzi']//li"
+    list_extract_field = {
+        'link': ".//@href",
+        'datetime': ".//td[@width='14%']/text()",
+    }
+
+    item_extract_scope = "//div[@class='nyzi']"
+    item_extract_field = {
+        'category': "//div[@class='nybtnright']/text()",
+        'title': ".//div[1]/text()",
+        'content': ".//div[2]",
+    }
+
+    datetime_format = "%Y-%m-%d"
+
+    def generate_item_id(self, url):
+        return self.name + util.extract_number(url, 1)
