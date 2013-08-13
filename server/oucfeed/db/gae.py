7136cd3818419afb4fc55869f012bccc73c95927
D6C92FE5 <D6C92FE5@hotmail.com>
2013-08-13 17:53:00 +0800

    使用 pickle 存储数据

--- server/oucfeed/db/gae.py
+++ server/oucfeed/db/gae.py
@@ -13,8 +13,8 @@ __all__ = [
 ]
 
 
-class JsonData(ndb.Model):
-    data = ndb.JsonProperty()
+class PickleData(ndb.Model):
+    data = ndb.PickleProperty()
 
 
 def _get_data(item, default=None):
@@ -25,29 +25,30 @@ def _get_data(item, default=None):
 
 
 def get_news():
-    return _get_data(JsonData.get_by_id(id="news"), [])
+    return _get_data(PickleData.get_by_id(id="news"), [])
 
 def set_news(news):
-    JsonData(id="news", data=news).put()
+    PickleData(id="news", data=news).put()
 
 
 def get_news_history():
-    return _get_data(JsonData.get_by_id(id="news_history"), [])
+    return _get_data(PickleData.get_by_id(id="news_history"), set())
 
-def set_news_history(news):
-    JsonData(id="news_history", data=news).put()
+def set_news_history(news_history):
+    PickleData(id="news_history", data=news_history).put()
 
 
 def get_category():
-    return _get_data(JsonData.get_by_id(id="category"), {})
+    return _get_data(PickleData.get_by_id(id="category"), {})
 
-def set_category(news):
-    JsonData(id="category", data=news).put()
+def set_category(category):
+    PickleData(id="category", data=category).put()
 
 
 def get_profile(id_):
-    parent = ndb.Key(JsonData, 'profile')
-    return _get_data(JsonData.get_by_id(id=id_, parent=parent), {})
+    parent = ndb.Key(PickleData, 'profile')
+    return _get_data(PickleData.get_by_id(id=id_, parent=parent), {})
 
 def set_profile(id_, profile):
-    JsonData(id=id_, parent=ndb.Key(JsonData, 'profile'), data=profile).put()
+    parent = ndb.Key(PickleData, 'profile')
+    PickleData(id=id_, parent=parent, data=profile).put()
