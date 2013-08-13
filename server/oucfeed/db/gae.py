24192f2da54c542b253f59210522b3c0ecc84204
D6C92FE5 <D6C92FE5@hotmail.com>
2013-08-13 13:17:26 +0800

    添加新闻时不添加曾经添加过和重复的项

--- server/oucfeed/db/gae.py
+++ server/oucfeed/db/gae.py
@@ -7,6 +7,7 @@ from google.appengine.ext import ndb
 
 __all__ = [
     "get_news", "set_news",
+    "get_news_history", "set_news_history",
     "get_category", "set_category",
     "get_profile", "get_profile",
 ]
@@ -28,6 +29,13 @@ def get_news():
 
 def set_news(news):
     JsonData(id="news", data=news).put()
+
+
+def get_news_history():
+    return _get_data(JsonData.get_by_id(id="news_history"), [])
+
+def set_news_history(news):
+    JsonData(id="news_history", data=news).put()
 
 
 def get_category():
--- server/oucfeed/news.py
+++ server/oucfeed/news.py
@@ -15,6 +15,13 @@ class News(object):
     @cherrypy.tools.json_in()
     def POST(self):
         news = db.get_news()
-        news.append(request.json)
+        history = set(db.get_news_history())
+
+        news_new = {x['id']: x for x in request.json if x['id'] not in history}
+        news.extend(news_new.itervalues())
+        history.update(news_new.iterkeys())
+
         db.set_news(news[-1000:])
+        db.set_news_history(list(history))
+
         return ""
