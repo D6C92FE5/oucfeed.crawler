80256084c8b3b03562736ba0e0d7b52e61531805
D6C92FE5 <D6C92FE5@hotmail.com>
2013-08-13 23:08:27 +0800

    实现分类相关的功能

--- server/oucfeed/category.py
+++ server/oucfeed/category.py
@@ -5,10 +5,39 @@ from __future__ import division, print_function, unicode_literals
 import cherrypy
 from cherrypy import request, response
 
-import db
+from oucfeed import db
 
 
 class Category(object):
 
     exposed = True
 
+    @cherrypy.tools.json_out()
+    def GET(self):
+        return db.get_category()
+
+    @cherrypy.tools.json_in()
+    def POST(self):
+        db.set_category(request.json)
+        return ""
+
+
+def add(category_iter):
+    category_dict = db.get_category()
+    for category in category_iter:
+        category_node = category_dict
+        for part in category:
+            if part not in category_node:
+                category_node[part] = {}
+            category_node = category_node[part]
+    db.set_category(category_dict)
+
+
+def match(category_dict, category):
+    for part in category:
+        category_dict = category_dict.get(part)
+        if category_dict == {}:
+            return True
+        if not category_dict:
+            break
+    return bool(category_dict)
--- server/oucfeed/news.py
+++ server/oucfeed/news.py
@@ -5,7 +5,7 @@ from __future__ import division, print_function, unicode_literals
 import cherrypy
 from cherrypy import request, response
 
-import db
+from oucfeed import db, category
 
 
 class News(object):
@@ -14,14 +14,22 @@ class News(object):
 
     @cherrypy.tools.json_in()
     def POST(self):
-        news = db.get_news()
-        history = set(db.get_news_history())
-
-        news_new = {x['id']: x for x in request.json if x['id'] not in history}
-        news.extend(news_new.itervalues())
-        history.update(news_new.iterkeys())
-
-        db.set_news(news[-1000:])
-        db.set_news_history(list(history))
-
+        add(request.json)
         return ""
+
+
+def add(news_iter):
+    history = db.get_news_history()
+
+    news_new = {x['id']: x for x in news_iter if x['id'] not in history}
+    for news in news_new.itervalues():
+        news['category'] = tuple(news['category'].split("/"))
+
+    history.update(news_new.iterkeys())
+    db.set_news_history(history)
+
+    news = db.get_news()
+    news.extend(news_new.itervalues())
+    db.set_news(news[-1000:])
+
+    category.add(x['category'] for x in news_new.itervalues())
